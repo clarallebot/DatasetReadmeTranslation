973c67bc8f1c05ea077f8aadafde664786cd1d77
carakey <keyca@oregonstate.edu>
2022-01-19 14:53:44 -0800

    use explicit index position for array item

--- cara_test.py
+++ cara_test.py
@@ -1,5 +1,22 @@
+from datetime import date
 import json
 
 with open('example_data.json') as json_file:
     data = json.load(json_file)
-    print(data['title'])
+    today = date.today()
+    today = today.strftime("%Y-%m-%d")
+
+text = (
+    # header
+    f"This documentation file was generated on {today}.\n\n"
+    "-------------------\n# GENERAL INFORMATION\n-------------------\n\n"
+
+    "1. Title of Dataset\n"
+    f"{data['title'][0]}\n\n"
+    
+    "2. Creator Information\n\n"
+    f"Name: {data['creator'][0]}"
+    )
+
+with open('output_readme.txt', 'w') as readme:
+    readme.write(text)
